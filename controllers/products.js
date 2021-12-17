5de3cdc7f1cf78dad4637735e4b9f19cdbd192fc
zyar3k <a.zygan@gmail.com>
2021-12-17 18:26:45 +0100

    Add select/fields features

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,13 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("name");
+  const products = await Product.find({}).select("name price");
 
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
   if (featured) queryObject.featured = featured === "true" ? true : false;
@@ -16,14 +16,21 @@ const getAllProducts = async (req, res) => {
 
   //   console.log(queryObject);
   let result = Product.find(queryObject);
+  //   sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
   }
-  const products = await result;
 
+  //   fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
