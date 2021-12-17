b4113e9b97ccdaffba8084a7814841f5a6a202b2
zyar3k <a.zygan@gmail.com>
2021-12-17 18:22:35 +0100

    Add sort features

--- controllers/products.js
+++ controllers/products.js
@@ -1,23 +1,29 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "ab";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+  const products = await Product.find({}).sort("name");
 
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
 
   const queryObject = {};
   if (featured) queryObject.featured = featured === "true" ? true : false;
   if (company) queryObject.company = company;
   if (name) queryObject.name = { $regex: name, $options: "i" };
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+
+  //   console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
+
   res.status(200).json({ nbHits: products.length, products });
 };
 
