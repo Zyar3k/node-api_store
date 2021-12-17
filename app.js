b076d60650f93efdbb0984adee5c674cd255ce78
zyar3k <a.zygan@gmail.com>
2021-12-17 16:48:57 +0100

    Add model

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const connectDB = require("./db/connect");
 const productsRouter = require("./routes/products");
 const notFoundMiddleware = require("./middleware/not-found");
--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,10 @@
 const getAllProductsStatic = async (req, res) => {
+  throw new Error("testing async error handling");
   res.status(200).json({ msg: "products testing route" });
-
-  //   try {
-  //     const products = await Product.find();
-  //     res.status(200).json({
-  //       success: true,
-  //       count: products.length,
-  //       data: products,
-  //     });
-  //   } catch (e) {
-  //     res.status(500).json({
-  //       success: false,
-  //       error: e,
-  //     });
-  //   }
 };
+
 const getAllProducts = async (req, res) => {
+  throw new Error("async error handling");
   res.status(200).json({ msg: "products route" });
 };
 
