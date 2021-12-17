8de87af23711f49624e7fc16f4034032e93f5f43
zyar3k <a.zygan@gmail.com>
2021-12-17 18:35:40 +0100

    Add pagination/limit

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select("name price");
+  const products = await Product.find({}).sort("name").select("name price");
 
   res.status(200).json({ nbHits: products.length, products });
 };
@@ -16,6 +16,7 @@ const getAllProducts = async (req, res) => {
 
   //   console.log(queryObject);
   let result = Product.find(queryObject);
+
   //   sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
@@ -29,6 +30,13 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+
+  //  pagination/limit
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
