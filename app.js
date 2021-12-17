156c3057ece9479a164b16c92cfffbab654661aa
zyar3k <a.zygan@gmail.com>
2021-12-17 15:51:42 +0100

    Init serrver & connect to db

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("starting app");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const connectDB = require("./db/connect");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>Products</a>");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const start = async () => {
+  try {
+    //   connectDB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log("Server is running on port " + PORT));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
